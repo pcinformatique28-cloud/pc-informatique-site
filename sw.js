2ebcdc426fa9124b6666e372b298f6017625bbf4
pcinformatique28-cloud <pcinformatique28@gmail.com>
2026-09-26 09:20:25 +0100

    Ajout PWA: manifest.json, service worker et icones installables

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "pc-informatique-v1";
+const CORE_ASSETS = [
+  "./index.html",
+  "./images/icon-192.png",
+  "./images/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).catch(() => cached);
+    })
+  );
+});
